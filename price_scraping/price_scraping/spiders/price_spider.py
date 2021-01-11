c6944419aa219afae1909fa14c2023bbc4383fb3
Robert Constable <rjconstable1989@gmail.com>
2021-01-11 20:51:45 +0000

    work

--- price_scraping/price_scraping/spiders/price_spider.py
+++ price_scraping/price_scraping/spiders/price_spider.py
@@ -6,15 +6,16 @@ from scrapy.utils.markup import remove_tags
 
 class Ecommerce(scrapy.Spider):
    name = "ecommerce"
-   start_urls = ["https://www.ebuyer.com/store/Computer/cat/Monitors",]
+   allowed_domains = ['www.ebuyer.com']
+   start_urls = ["https://www.ebuyer.com/store/Computer/cat/Monitors"]
 
    def parse(self, response):
-       item_loader = ItemLoader(item=ProductItem(), response=response)
-       item_loader.default_input_processor = MapCompose(remove_tags)
+      l = ItemLoader(item=ProductItem(), response=response)
+      item_loader.default_input_processor = MapCompose(remove_tags)
 
-       item_loader.add_css("product", "h1[itemprop='name']")
-       item_loader.add_css("price", "span[itemprop=price]")
-       item_loader.add_css("stock", "span[itemprop=’stock’]")
-       item_loader.add_css("category", "a[data-track='Breadcrumb']")
-       print(item_loader.load_item()) 
-       return item_loader.load_item()
+      l.add_xpath("product", '//p[@class="drop-product-title"]')
+      #  #item_loader.add_css("price", "data-product-price")
+      #  l.add_xpath('site_title', '//title/text()')
+      l.add_xpath('link', '//a/@href')
+
+      return l.load_item()
